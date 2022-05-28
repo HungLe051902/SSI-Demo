eac8dd63048c794297462ebe532078fa45d6f69a
Nam Tran Van <nam.tranvan@petronas.com>
2022-05-28 11:20:44 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
+import axios from 'axios';
+
+const baseURL = `${process.env.REACT_APP_BASE_URL}/connections/create-invitation`;
 
 function App() {
+  React.useEffect(() => {
+    console.log("base url", process.env.REACT_APP_BASE_URL);
+    
+    axios.post(baseURL, {}).then((response) => {
+      console.log("HHHHHHHH2", response);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
@@ -50,9 +61,20 @@ function App() {
             React Redux
           </a>
         </span>
-      </header>
+      </header> */}
+
+      <h2>Test Section</h2>
+      <div style={styles.test}>
+        <button>Test SSI</button>
+      </div>
     </div>
   );
 }
+
+const styles = {
+  test: {
+   marginBottom: 50
+  },
+} as const;
 
 export default App;
